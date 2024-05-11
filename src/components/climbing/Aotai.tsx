f9f49265be14caa7dd7a8824f1aa9aa4641618a2
Aqrun <aqrun@sina.com>
2024-05-11 16:53:57 +0800

    feat: image view modify

--- src/components/climbing/Aotai.tsx
+++ src/components/climbing/Aotai.tsx
@@ -5,23 +5,25 @@ import { PhotoProvider,PhotoView } from 'react-photo-view';
 
 export const Aotai = () => {
   return (
-    <PhotoProvider>
+    <>
       <div className="mt-4">
         <p className=" mb-2 text-gray-500">
           鳌太路线手绘示意图, 仅供参考。
         </p>
         <div>
-          <PhotoView
-            src="https://cdn.oicnp.com/images/2024/aotai.jpg"
-          >
-            <Image
-              src='https://cdn.oicnp.com/images/2024/aotai.jpg'
-              alt=''
-              className='inline-block h-full w-full object-cover'
-              width={1280}
-              height={960}
-            />
-          </PhotoView>
+          <PhotoProvider>
+            <PhotoView
+              src="https://cdn.oicnp.com/images/2024/aotai.jpg"
+            >
+              <Image
+                src='https://cdn.oicnp.com/images/2024/aotai.jpg'
+                alt=''
+                className='inline-block h-full w-full object-cover'
+                width={1280}
+                height={960}
+              />
+            </PhotoView>
+          </PhotoProvider>
         </div>
       </div>
 
@@ -30,17 +32,19 @@ export const Aotai = () => {
           秦岭经典路线
         </p>
         <div>
-          <PhotoView
-            src="https://cdn.oicnp.com/images/2024/classic_roads.jpg"
-          >
-            <Image
-              src='https://cdn.oicnp.com/images/2024/classic_roads.jpg'
-              alt=''
-              className='inline-block h-full w-full object-cover'
-              width={1280}
-              height={1708}
-            />
-          </PhotoView>
+          <PhotoProvider>
+            <PhotoView
+              src="https://cdn.oicnp.com/images/2024/classic_roads.jpg"
+            >
+              <Image
+                src='https://cdn.oicnp.com/images/2024/classic_roads.jpg'
+                alt=''
+                className='inline-block h-full w-full object-cover'
+                width={1280}
+                height={1708}
+              />
+            </PhotoView>
+          </PhotoProvider>
         </div>
       </div>
 
@@ -49,20 +53,22 @@ export const Aotai = () => {
           西安户外爬山鄙视链
         </p>
         <div>
-          <PhotoView
-            src="https://cdn.oicnp.com/images/2024/qinlinglian.jpg"
-          >
-            <Image
-              src='https://cdn.oicnp.com/images/2024/qinlinglian.jpg'
-              alt=''
-              className='inline-block h-full w-full object-cover'
-              width={1170}
-              height={1556}
-            />
-          </PhotoView>
+          <PhotoProvider>
+            <PhotoView
+              src="https://cdn.oicnp.com/images/2024/qinlinglian.jpg"
+            >
+              <Image
+                src='https://cdn.oicnp.com/images/2024/qinlinglian.jpg'
+                alt=''
+                className='inline-block h-full w-full object-cover'
+                width={1170}
+                height={1556}
+              />
+            </PhotoView>
+          </PhotoProvider>
         </div>
       </div>
-    </PhotoProvider>
+    </>
   );
 };
 
